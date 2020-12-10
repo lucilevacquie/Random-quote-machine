6079b93599d4fc58a1551339da29998748df8d41
Lucile Vacquie <lucilevacquie@luciles-mbp.mynet>
2020-12-10 13:55:29 +0000

    finished project

--- src/App.js
+++ src/App.js
@@ -1,25 +1,64 @@
-import logo from './logo.svg';
 import './App.css';
+import React, {useState} from "react";
 
-function App() {
+const QuotesLibrary = [
+  {
+    key: 0,
+    text:"Be the change that you wish to see in the world.",
+    author:"Gandhi"
+  },
+  {
+    key: 1,
+    text:"I have no special talent. I am only passionately curious.",
+    author:"Albert Einstein"
+  },
+  {
+    key: 2,
+    text:"If you judge people, you have no time to love them.",
+    author:"Mother Teresa"
+  },
+  {
+    key: 3,
+    text:"Don't count the days, make the days count.",
+    author:"Muhammad Ali"
+  },
+  {
+    key: 4,
+    text:"Be kind whenever possible. It is always possible.",
+    author:"Dalai Lama"
+  },
+]
+
+const RandomQuote = () => {
+
+  const [currentQuote, setCurrentQuote] = useState({})
+
+  const getRandomQuote = () => {
+    const randomInt = (min, max) => {
+    return Math.floor(Math.random() * (max - min + 1)) + min
+    }
+    const minQuotes = 0
+    let maxQuotes = QuotesLibrary.length - 1
+
+    let int = randomInt(minQuotes, maxQuotes)
+
+    setCurrentQuote({
+      text: QuotesLibrary[int].text,
+      author: QuotesLibrary[int].author
+    })
+
+  }
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+  <div id="quote-box">
+    <h1>Random Quote Machine</h1>
+    <div id="text">"{currentQuote.text}"</div>
+    <div id="author">{currentQuote.author}</div>
+    <button id="new-quote" onClick={() => getRandomQuote()}>New quote</button>
+    <br/>
+    <a id="tweet-quote" href="twitter.com/intent/tweet" target="_blank">Tweet this quote</a>
+  </div>
+  )
 }
 
-export default App;
+export default RandomQuote;
